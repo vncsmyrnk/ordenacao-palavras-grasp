8ef3a965c9410194b5c8e351af2b0481a1232849
vncsmyrnk <vmayrink12@gmail.com>
2023-06-19 08:30:50 -0300

    Incluindo alteracoes finais

--- src/App.java
+++ src/App.java
@@ -1,17 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import Analisador.Arquivo;
+import MochilaPalavras.Mochila;
 
 public class App {
     public static void main(String[] args) throws Exception {
         long startTime, estimatedTime;
-        final String[] filePaths = { "./data/palavras_100k.txt", "./data/palavras_200k.txt", "./data/palavras_300k.txt",
-                "./data/palavras_400k.txt", "./data/palavras_500k.txt", "./data/palavras_600k.txt",
-                "./data/palavras_700k.txt" };
-        final String[] outputFilePaths = { "./output/palavras_100k.txt", "./output/palavras_200k.txt",
-                "./output/palavras_300k.txt",
-                "./output/palavras_400k.txt", "./output/palavras_500k.txt", "./output/palavras_600k.txt",
-                "./output/palavras_700k.txt" };
+        final String[] filePaths = {
+                "./data/palavras_100k.txt", "./data/palavras_100k.txt", "./data/palavras_100k.txt",
+                "./data/palavras_200k.txt", "./data/palavras_200k.txt", "./data/palavras_200k.txt",
+                "./data/palavras_300k.txt", "./data/palavras_300k.txt", "./data/palavras_300k.txt",
+                "./data/palavras_400k.txt", "./data/palavras_400k.txt", "./data/palavras_400k.txt",
+                "./data/palavras_500k.txt", "./data/palavras_500k.txt", "./data/palavras_500k.txt",
+                "./data/palavras_600k.txt", "./data/palavras_600k.txt", "./data/palavras_600k.txt",
+                "./data/palavras_700k.txt", "./data/palavras_700k.txt", "./data/palavras_700k.txt" };
+        final String[] outputFilePaths = {
+                "./output/palavras_100k_1.txt", "./output/palavras_100k_2.txt", "./output/palavras_100k_3.txt",
+                "./output/palavras_200k_1.txt", "./output/palavras_200k_2.txt", "./output/palavras_200k_3.txt",
+                "./output/palavras_300k_1.txt", "./output/palavras_300k_2.txt", "./output/palavras_300k_3.txt",
+                "./output/palavras_400k_1.txt", "./output/palavras_400k_2.txt", "./output/palavras_400k_3.txt",
+                "./output/palavras_500k_1.txt", "./output/palavras_500k_2.txt", "./output/palavras_500k_3.txt",
+                "./output/palavras_600k_1.txt", "./output/palavras_600k_2.txt", "./output/palavras_600k_3.txt",
+                "./output/palavras_700k_1.txt", "./output/palavras_700k_2.txt", "./output/palavras_700k_3.txt" };
         final String outputFileTerminationFrequencia = "_frequencia";
         final String outputFileTerminationOrdenacao = "_ordenacao";
+
+        System.out.println("Primeira etapa. Ordenação de palavras dos arquivos.");
 
         for (int i = 0; i < filePaths.length; i++) {
             String filePath = filePaths[i];
@@ -20,9 +34,11 @@ public class App {
             /* Caminho do arquivo de saida de cada operacao */
 
             String[] filePathSplitted = outputPath.substring(1).split("\\.");
-            String outputPathOrdenacao = "." + filePathSplitted[0] + outputFileTerminationOrdenacao
+            String outputPathOrdenacao = "." + filePathSplitted[0] +
+                    outputFileTerminationOrdenacao
                     + "." + filePathSplitted[1];
-            String outputPathFrequencia = "." + filePathSplitted[0] + outputFileTerminationFrequencia
+            String outputPathFrequencia = "." + filePathSplitted[0] +
+                    outputFileTerminationFrequencia
                     + "." + filePathSplitted[1];
 
             /* Ordenacao */
@@ -34,7 +50,8 @@ public class App {
 
             a.ordenacaoLexografica();
             estimatedTime = (System.currentTimeMillis() - startTime) / 1000;
-            System.out.println("Tempo total ordenação arquivo " + filePath + ": " + estimatedTime + "s");
+            System.out.println("Tempo total ordenação arquivo " + filePath + ": " +
+                    estimatedTime + "s");
 
             System.out.println("Gerando arquivo com o resultado da ordenação...");
             a.geraArquivoPalavras(outputPathOrdenacao, false);
@@ -44,16 +61,29 @@ public class App {
 
             Arquivo b = new Arquivo(filePath);
 
-            System.out.println("Calculando frequência das palavras do arquivo " + filePath + "...");
+            System.out.println("Calculando frequência das palavras do arquivo " +
+                    filePath + "...");
             startTime = System.currentTimeMillis();
 
             b.frequencia();
             estimatedTime = (System.currentTimeMillis() - startTime) / 1000;
-            System.out.println("Tempo total ordenação arquivo " + filePath + ": " + estimatedTime + "s");
+            System.out.println("Tempo total ordenação arquivo " + filePath + ": " +
+                    estimatedTime + "s");
 
             System.out.println("Gerando arquivo com o resultado da frequência...");
             b.geraArquivoPalavras(outputPathFrequencia, true);
             System.out.println("Arquivo gerado: " + outputPathFrequencia);
+        }
+
+        System.out.println(
+                "Segunda Etapa. Calculando valores máximos para o problema da mochila com as informações do arquivo 'palavras_100k.txt'. São consideradas as 50 palavras mais frequentes do arquivo considerando a frequência como o valor e o peso como a expansão de \u03C0.");
+
+        Arquivo a = new Arquivo("./data/palavras_100k.txt");
+        Mochila m = new Mochila(a);
+
+        for (int i = 50; i <= 80; i++) {
+            System.out.println("Encontrando o valor máximo para um mochila de peso (W) como " + i + "...");
+            System.out.println("Valor máximo: " + m.valorMaximo(i));
         }
     }
 }
